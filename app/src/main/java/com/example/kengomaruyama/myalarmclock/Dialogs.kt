3e1f3277303a494db34a6597f697377784a57f58
klcatha <klcatha0518@gmail.com>
2018-11-08 12:56:30 +0900

    simple alert dialog class

--- app/src/main/java/com/example/kengomaruyama/myalarmclock/Dialogs.kt
+++ app/src/main/java/com/example/kengomaruyama/myalarmclock/Dialogs.kt
@@ -1,2 +1,28 @@
 package com.example.kengomaruyama.myalarmclock
 
+import android.app.AlertDialog
+import android.app.Dialog
+import android.app.DialogFragment
+import android.os.Build
+import android.os.Bundle
+import android.support.annotation.RequiresApi
+import org.jetbrains.anko.toast
+
+class SimpleAlertDialog : DialogFragment(){
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val context = context
+        if(context == null)
+            return super.onCreateDialog(savedInstanceState)
+        val builder = AlertDialog.Builder(context).apply {
+            setMessage("時間になりました")
+            setPositiveButton("起きる"){dialog, which ->
+                context.toast("起きるがクリックされました")
+            }
+            setNegativeButton("あと5分"){dialog, which ->
+                context.toast("あと5分がクリックされました")
+            }
+        }
+        return builder.create()
+    }
+}
