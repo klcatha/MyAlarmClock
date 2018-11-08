4e02a957e07d58b26aee9d0aea080f315f533933
klcatha <klcatha0518@gmail.com>
2018-11-08 12:28:55 +0900

    set alarm manager intent

--- app/src/main/java/com/example/kengomaruyama/myalarmclock/MainActivity.kt
+++ app/src/main/java/com/example/kengomaruyama/myalarmclock/MainActivity.kt
@@ -1,12 +1,49 @@
 package com.example.kengomaruyama.myalarmclock
 
+import android.annotation.TargetApi
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setAlarm.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            calendar.timeInMillis = System.currentTimeMillis()
+            calendar.add(Calendar.SECOND, 5)
+            setAlarmManager(calendar)
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun setAlarmManager(calendar: Calendar){
+        val am = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(this, AlarmBroadcastReceiver::class.java)
+        val pending = PendingIntent.getBroadcast(this, 0, intent,0)
+        when{
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP -> {
+                val info = AlarmManager.AlarmClockInfo(
+                        calendar.timeInMillis, null)
+                am.setAlarmClock(info,pending)
+            }
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT -> {
+                am.setExact(AlarmManager.RTC_WAKEUP,
+                        calendar.timeInMillis, pending)
+            }
+            else -> {
+                am.set(AlarmManager.RTC_WAKEUP,
+                        calendar.timeInMillis, pending)
+            }
+        }
     }
 }
