7961a5a6b6998c63f850d0c3553561b07861544a
klcatha <klcatha0518@gmail.com>
2018-11-08 12:11:39 +0900

    alarm broadcast receiver onReceive method

--- app/src/main/java/com/example/kengomaruyama/myalarmclock/AlarmBroadcastReceiver.kt
+++ app/src/main/java/com/example/kengomaruyama/myalarmclock/AlarmBroadcastReceiver.kt
@@ -1,4 +1,12 @@
 package com.example.kengomaruyama.myalarmclock
 
-class AlarmBroadcastReceiver {
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import org.jetbrains.anko.toast
+
+class AlarmBroadcastReceiver: BroadcastReceiver(){
+    override fun onReceive(context: Context?, intent: Intent?) {
+        context?.toast("アラームを受信しました")
+    }
 }
