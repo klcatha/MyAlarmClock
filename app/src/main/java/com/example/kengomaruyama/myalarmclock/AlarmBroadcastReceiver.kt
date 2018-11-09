d2b4ea99cdc01c5a52167b694e6e5d276c4c47cb
klcatha <klcatha0518@gmail.com>
2018-11-09 13:00:33 +0900

    broadcast receiver onReceive

--- app/src/main/java/com/example/kengomaruyama/myalarmclock/AlarmBroadcastReceiver.kt
+++ app/src/main/java/com/example/kengomaruyama/myalarmclock/AlarmBroadcastReceiver.kt
@@ -7,6 +7,9 @@ import org.jetbrains.anko.toast
 
 class AlarmBroadcastReceiver: BroadcastReceiver(){
     override fun onReceive(context: Context?, intent: Intent?) {
-        context?.toast("アラームを受信しました")
+        val intent = Intent(context, MainActivity::class.java)
+                .putExtra("onReceive", true)
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        context?.startActivity(intent)
     }
 }
