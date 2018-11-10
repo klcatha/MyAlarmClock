61ccb90b6ed65b5581663e702fc78d5e7639cf7b
klcatha <klcatha0518@gmail.com>
2018-11-10 12:01:19 +0900

    date dialog

--- app/src/main/java/com/example/kengomaruyama/myalarmclock/Dialogs.kt
+++ app/src/main/java/com/example/kengomaruyama/myalarmclock/Dialogs.kt
@@ -1,13 +1,18 @@
 package com.example.kengomaruyama.myalarmclock
 
 import android.app.AlertDialog
+import android.app.DatePickerDialog
 import android.app.Dialog
 import android.app.DialogFragment
 import android.content.Context
 import android.os.Build
 import android.os.Bundle
 import android.support.annotation.RequiresApi
+import android.widget.DatePicker
 import org.jetbrains.anko.toast
+import java.time.Month
+import java.time.Year
+import java.util.*
 
 class SimpleAlertDialog : DialogFragment(){
     @RequiresApi(Build.VERSION_CODES.M)
@@ -39,4 +44,35 @@ class SimpleAlertDialog : DialogFragment(){
         }
         return builder.create()
     }
+}
+
+class DatePickerFragment : DialogFragment(),
+        DatePickerDialog.OnDateSetListener {
+
+    interface OnDateSelectedListener {
+        fun onSelected(year: Int, month: Int, date:Int)
+    }
+
+    private lateinit var listener: OnDateSelectedListener
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        if (context is OnDateSelectedListener){
+            listener = context
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val date = c.get(Calendar.DAY_OF_MONTH)
+        return DatePickerDialog(context, this, year,month,date)
+    }
+
+    override fun onDateSet(view: DatePicker, year: Int, month: Int, date: Int) {
+        listener.onSelected(year,month,date)
+    }
+
 }
